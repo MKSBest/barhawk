6d1fffe1d64c284faf40797cc0f6dfa986335a19
JohnChau <ydjjabt@gmail.com>
2000-12-31 16:06:02 -0800

    (feat) Added picture upload to new user signup page.

--- client/app/auth/customer/customerAuth.js
+++ client/app/auth/customer/customerAuth.js
@@ -1,6 +1,6 @@
 angular.module('asyncdrink.customerAuth', [])
 
-.controller('customerController', function ($scope, $state, $window, customerFactory, optionsFactory) {
+.controller('customerController', function($scope, $state, $window, customerFactory, optionsFactory) {
 
   //newUser obj will hold all sign up inputs and set drinkCount to 0
   $scope.newUser = {
@@ -12,16 +12,31 @@ angular.module('asyncdrink.customerAuth', [])
 
   $scope.invalidLogIn = false;
 
-  $scope.clear = function () {
+  $scope.clear = function() {
     $scope.invalidLogIn = false;
   };
+  var globalPicture;
+  $scope.previewUploadImage = function() {
+    var fileInput = document.getElementById('fileInput');
+    var file = fileInput.files[0];
+    var img = document.getElementById('uploadImage');
 
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      img.src = reader.result;
+      globalPicture = reader.result;
+    }
+    reader.readAsDataURL(file);
+  }
 
-  $scope.signUp = function () {
-
+  $scope.signUp = function() {
+    console.log('globalPicture is ', globalPicture);
+    console.log('$scope.newUser is ', $scope.newUser );
+    $scope.newUser.photo = globalPicture;
     customerFactory.signUp($scope.newUser)
-      .then(function (response) {
+      .then(function(response) {
         //hide error message, if displayed
+        console.log('anything occur here???')
         $scope.invalidSignup = false;
 
         optionsFactory.currentUser = response.currentUser;
@@ -31,16 +46,16 @@ angular.module('asyncdrink.customerAuth', [])
         //navigate to options page
         $state.go('options');
       })
-      .catch(function (error) {
+      .catch(function(error) {
         //show user that they failed to signup successfully
         $scope.invalidSignup = true;
       });
   };
 
-  $scope.logIn = function () {
+  $scope.logIn = function() {
 
     customerFactory.signIn($scope.loginAttempt)
-      .then(function (response) {
+      .then(function(response) {
         //hide error message, if displayed
         $scope.invalidLogIn = false;
         //persist logged in user
@@ -50,38 +65,38 @@ angular.module('asyncdrink.customerAuth', [])
         //navigate to options page
         $state.go('options');
       })
-      .catch(function (error) {
+      .catch(function(error) {
         //display invalid login message
         $scope.invalidLogIn = true;
       });
   };
 })
 
-.factory('customerFactory', function ($http, $window) {
-  var signIn = function (loginAttempt) {
+.factory('customerFactory', function($http, $window) {
+  var signIn = function(loginAttempt) {
     return $http({
         method: "POST",
         url: '/api/users/login',
         data: loginAttempt
       })
-      .then(function (resp) {
+      .then(function(resp) {
         return resp.data;
       })
   };
 
-  var signUp = function (userInfo) {
+  var signUp = function(userInfo) {
     console.log('we are inside signUP HTTP');
     return $http({
         method: "POST",
         url: '/api/users/signup',
         data: userInfo
       })
-      .then(function (resp) {
+      .then(function(resp) {
         return resp.data;
       });
   };
 
-  var isAuth = function () {
+  var isAuth = function() {
     return !!$window.localStorage.getItem('com.barhawk');
 
     //the below is a way to check if token exists and is correct for that particular user
